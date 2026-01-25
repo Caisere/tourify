f589bee02fee30c552ec8130d93e5bf84d217e11
Omoshola <heylehgbedey@gmail.com>
2026-01-25 07:09:51 -0800

    initial commit

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const fs = require('fs')
+
+const app = express()
+
+const PORT = 8080
+
+// fine to read using sync format. top level code run before event loop starts
+const toursData = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours.json`, 'utf-8'))
+
+
+app.get('/', (req, res) => {
+    // console.log(toursData)
+    res.status(200).json({
+        message: 'success',
+        data: {
+            tours: toursData
+        }
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`Tourify server started at port ${PORT}`)
+})
