c75b757d10f0d8425f4a0825f37538f3df943910
Omoshola <heylehgbedey@gmail.com>
2026-01-25 10:42:56 -0800

    feat: Add POST endpoint for creating new tours and implement JSON middleware

--- src/app.js
+++ src/app.js
@@ -3,19 +3,49 @@ const fs = require('fs')
 
 const app = express()
 
+// middleware
+app.use(express.json())
+
 const PORT = 8080
 
 // fine to read using sync format. top level code run before event loop starts
 const toursData = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours.json`, 'utf-8'))
 
 
+// GET
 app.get('/api/v1/tours', (req, res) => {
-    // console.log(toursData)
     res.status(200).json({
         message: 'success',
         count: toursData.length,
         data: {
             tours: toursData
+        }
+    })
+})
+
+// POST
+app.post('/api/v1/tours', (req, res) => {
+    const newId = toursData.length + 1;
+
+    console.log(newId)
+    
+    const newTour = Object.assign({id: newId}, req.body)
+
+    toursData.push(newTour)
+
+    fs.writeFile(`${__dirname}/dev-data/data/tours.json`, JSON.stringify(toursData), err => {
+
+        if (err) {
+            res.status(400).json({
+                error: 'Error while writing to file'
+            })
+        }
+    })
+
+    res.status(201).json({
+        status: 'success',
+        data: {
+            tour: newTour
         }
     })
 })
